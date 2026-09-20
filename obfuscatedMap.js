a922135a4bad93186a4cc8cfcfc2956352029867
jason666-ux <slmovieuniverse@gmail.com>
2026-09-20 20:48:07 +0530

    Update video map (Video Manager)

--- obfuscatedMap.js
+++ obfuscatedMap.js
@@ -312,5 +312,6 @@ const obfuscatedMap = {
   "m1abdx": "BQADAQADohQAAhkFYEWC_jCIuAPUGRYE",
   "2x3w08": "BQADAQADtRQAAhkFYEXqWFmVYer4rBYE",
   "307w18": "BQADAQADwBQAAhkFYEW6oJijh_o9dhYE",
-  "f1i25b": "BQADAQADxBAAAo5qgEWhu71Gk1QmZxYE"
+  "f1i25b": "BQADAQADxBAAAo5qgEWhu71Gk1QmZxYE",
+  "aejawj": "BQADAQAD8REAAo5qgEXkcqG9QvycjxYE"
 };
